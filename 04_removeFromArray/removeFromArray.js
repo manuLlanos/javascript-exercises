3ee2055bde5d1f2c5cf9dc09f46e1f0736a6878c
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-12 21:22:18 -0300

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    let result = [];
+    for(let arrayElement of array) {
+        if(!values.includes(arrayElement)) {
+            result.push(arrayElement);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
