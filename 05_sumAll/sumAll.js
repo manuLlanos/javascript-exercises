9e2acc86b164d9e84faa516bf42ec31d61b42f6d
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-12 21:37:58 -0300

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    for(let arg of arguments) {
+        if(arg < 0 || !Number.isInteger(arg)) {
+            return "ERROR";
+        }
+    }
 
+    if(start === end) {
+        return start;
+    }
+
+    if(end < start) {
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    if(start === 1) {
+        return gausSummation(end);
+    }
+    else {
+        return gausSummation(end) - gausSummation(start-1);
+    }
 };
+
+// 1 + 2 + 3 + ... + N = N*(N+1)/2
+function gausSummation(n) {
+    return n * (n+1) / 2;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
