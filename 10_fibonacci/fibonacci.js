4d30bbe8a29cdd29bbd91b9e1237983394f4bfaa
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-18 22:01:59 -0300

    Solve javascript 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    index = parseInt(index);
+    if (index < 0) return "OOPS";
+    if (index == 0) return 0;
+    if (index < 3) return 1;
 
+    let prev = 1;
+    let curr = 2;
+    for (let i = 3; i < index; i++) {
+        let tmp = curr;
+        curr = curr + prev;
+        prev = tmp;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
