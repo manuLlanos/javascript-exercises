6b7f40cce390c95068954731881d6f0dc127e6f6
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-18 22:24:20 -0300

    Solve javascript 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+    const currentYear = (new Date()).getFullYear();
+    
+    if (!person.yearOfDeath) {
+        return currentYear - person.yearOfBirth;
+    }
+    else return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+    return people.reduce(
+        (oldest, person) => {
+            return getAge(person) > getAge(oldest) ? person : oldest;
+        }, people[0]
+    );
 };
 
 // Do not edit below this line
