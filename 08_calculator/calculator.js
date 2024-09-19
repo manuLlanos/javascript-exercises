e7edc315eb0a98101308270d9f3fa2f4d7b8ed31
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-18 21:28:34 -0300

    Solve javascript-08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,12 +14,16 @@ const multiply = function (arr) {
   return arr.reduce((total, current) => total * current, 1);
 };
 
-const power = function () {
-
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function () {
-
+const factorial = function (n) {
+  let result = 1;
+  for (let i = n; i > 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
