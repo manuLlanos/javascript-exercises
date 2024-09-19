94b93f74cbd4c58df8d2353be8d4257af1c7276a
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-18 21:50:11 -0300

    Solve javascript 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let normalized = phrase
+        .toLowerCase()
+        .split("").filter(char => /[a-z0-9]/.test(char))
+        .join("");
 
+    let reversed = normalized.split("").reverse().join("");
+
+    return(normalized === reversed);
 };
 
 // Do not edit below this line
